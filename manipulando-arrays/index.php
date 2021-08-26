a04ad4805c0f5d5b187954fa7b6ffd89aeb91492
Sergio Ramos <sergio.ramos@unemat.br>
2021-08-26 16:10:26 -0400

    manipulando arrays

---
+++ manipulando-arrays/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$notaPortugues = 9;
+$notaMatematica = 3;
+$notaGeografia = 10;
+$notaHistoria = 5;
+$notaQuimica = 10;
+
+echo "A nota de português é: $notaPortugues";
+echo "</br>";
+echo "A nota de matemática é: $notaMatematica";
+echo "</br>";
+echo "A nota de geografia é: $notaGeografia";
+echo "</br>";
+echo "A nota de história é: $notaHistoria";
+echo "</br>";
+echo "A nota de história é: $notaQuimica";
+echo "</br>";
+
+$media = ($notaPortugues + $notaMatematica + $notaGeografia + $notaHistoria + $notaQuimica) / 5 ;
+
+echo "</br>";
+echo "A média é: $media";
